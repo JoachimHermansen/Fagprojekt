06e96809e8648fed1fac08111fb0c32ab988e17b
StefanPetro <43845091+StefanPetro@users.noreply.github.com>
2018-10-25 12:16:55 +0200

    Add files via upload

--- Archive and Burst/BurstSearch.py
+++ Archive and Burst/BurstSearch.py
@@ -2,6 +2,7 @@ from ArchiveExtract import *
 
 directory = "E03/"
 
+
 def burstSearch(directory):
 
     for filename in os.listdir("ArchiveCSV/"+directory):
@@ -13,21 +14,23 @@ def burstSearch(directory):
             burstdf = pd.DataFrame(np.zeros((N, len(archiveColumns))), columns=archiveColumns)
 
             for i in range(len(archivedf.iloc[:, 0])-1):
-                if (archivedf.iloc[i, 0] == archivedf.iloc[i+1, 0]) \
-                        or (int(archivedf.iloc[i, 0])-int(archivedf.iloc[i+1, 0]) == 1):
-                    if (float(archivedf.iloc[i, 1])-float(archivedf.iloc[i+1, 1]) <= 0.003) \
-                            or (float(archivedf.iloc[i, 1])-float(archivedf.iloc[i+1, 1]) <= -0.003):
-                        if (float(archivedf.iloc[i, 2])-float(archivedf.iloc[i+1, 2]) <= 0.003) \
-                                or (float(archivedf.iloc[i, 2])-float(archivedf.iloc[i+1, 2]) <= -0.003):
-                            if (float(archivedf.iloc[i+1, 6]) < float(archivedf.iloc[i, 5])
-                                    or float(archivedf.iloc[i+1, 5]) > float(archivedf.iloc[i, 6])):
-                                burstdf.iloc[i, :] = archivedf.iloc[i, :]
-                                burstdf.iloc[i+1, :] = archivedf.iloc[i+1, :]
+                if ( ((archivedf.iloc[i, 0] == archivedf.iloc[i+1, 0])
+                        or abs((int(archivedf.iloc[i, 0])%10e6 -int(archivedf.iloc[i+1, 0])%10e6) == 1))
+                        and (abs((float(archivedf.iloc[i, 1])-float(archivedf.iloc[i+1, 1]))) <= 0.003)
+                        and (abs((float(archivedf.iloc[i, 2])-float(archivedf.iloc[i+1, 2]))) <= 0.003)
+                        and (float(archivedf.iloc[i+1, 6]) < float(archivedf.iloc[i, 5])
+                        or float(archivedf.iloc[i+1, 5]) > float(archivedf.iloc[i, 6])) ):
+                            burstdf.iloc[i, :] = archivedf.iloc[i, :]
+                            burstdf.iloc[i+1, :] = archivedf.iloc[i+1, :]
+                else:
+                    pass
 
             ''' Sorter data (fjerner nul rækker) '''
             burstdf = burstdf[(burstdf.T != 0).any()]
 
-            burstdf.to_csv("BurstCSV/"+filename, sep=',', encoding='utf-8', index=False)
+            burstdf.to_csv("BurstCSV/"+directory+filename, sep=',', encoding='utf-8', index=False)
+            print("done")
+
 
 burstSearch(directory)
 
