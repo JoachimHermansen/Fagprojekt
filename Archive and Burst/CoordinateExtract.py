1236f6d7444852126a061241a07cb58801be8a27
LucasWith <43844565+LucasWith@users.noreply.github.com>
2018-11-22 12:59:44 +0100

    Add files via upload

---
+++ Archive and Burst/CoordinateExtract.py
@@ -0,0 +1,43 @@
+import pandas as pd
+
+directory = "E04/"
+
+file = "CombinedE04Combined.csv"
+
+name = ["EXO 0748-676","GS 0836-429","4U 1323-62","4U 1608-522",
+        "4U 1636-536","MXB 1658-298","4U 1705-44","XTE J1710-281",
+        "4U 1735-44","2S 1742-294","EXO 1745-248 (Tz 5)f","4U 1746-37 (NGC 6641)f",
+        "SAX J1747.0-2853","Aql X-1","Cyg X-2f"]
+x = [117.140460,129.348300,201.651290,243.179200,250.231300,255.527250,
+     257.226958,257.551300,264.742900,266.521667,267.021650,267.552750,
+     266.760830,287.816880,326.171477]
+y = [-67.752140,-42.900600,-62.136080,-52.423100,-53.751400,-29.945580,
+     -44.102042,-28.131700,-44.450000,-29.514806,-24.779833,-37.052280,
+     -28.883030,0.584940,38.321407]
+n = 14
+
+def CoorExtract(file,x,y):
+    
+    df=pd.read_csv("BurstCSV/"+directory+file)    
+    
+    df=df.loc[df['coordinate1']<x[n]+0.003]
+    df=df.loc[df['coordinate1']>x[n]-0.003]
+
+    df=df.loc[df['coordinate2']<y[n]+0.003]
+    df=df.loc[df['coordinate2']>y[n]-0.003]
+  
+    if df.empty:
+        print('DataFrame is empty!')
+    else:
+        df.to_csv("BurstCSV/"+directory+"BurstFound"+name[n]+".csv", sep=',', encoding="utf-8" , index=False)
+    
+    return df
+
+print(CoorExtract(file,x,y))
+                
+
+            
+            
+            
+            
+            
