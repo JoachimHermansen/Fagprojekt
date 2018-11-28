eddbc8a7ac6cfdadad05d743a965f377b27ee867
StefanPetro <43845091+StefanPetro@users.noreply.github.com>
2018-11-28 23:38:56 +0100

    New and updated

--- Archive and Burst/ArchiveExtract.py
+++ Archive and Burst/ArchiveExtract.py
@@ -3,29 +3,31 @@ import pandas as pd
 import re
 import os
 
-N = 1000  # amount of lines
+N = 20000  # amount of lines
 
 search = ["fk5;", "EnD", "Channel Width"]
-archiveColumns = ["SCW", "coordinate1", "coordinate2", "Name",
-                  "ObsTime[s]", "time chans start", "time chans stop",
-                  "Significance", "IJDStart[D]", "IJDStop[D]", "ChannelWidth[s]",
-                  "BurstStart[s]"]  # units in [], s = seconds, D = days
+archiveColumns = ["File", "SCW", "coordinate1", "coordinate2", "Name",
+                  "cm2", "ChannelWidth[s]", "Significance",
+                  "ObsTime[s]", "BurstStart[s]",
+                  "time chans start", "time chans stop",
+                  "IJDStart[D]", "IJDStop[D]"]  # units in [], s = seconds, D = days
 
-directory = "E03/" # Chosen Directory (E03 or E04)
+directory = "E03/"  # Chosen Directory (E03 or E04)
 
 
 def archiveExtract(directory):
+
+    archivedf = pd.DataFrame(np.zeros((N, len(archiveColumns))), columns=archiveColumns)
+    m = 0
+    n = 0
+    x = 0
+
     for filename in os.listdir("ArchiveFiles/"+directory):
         if filename.endswith(".txt"):
             with open(os.path.join("ArchiveFiles/"+directory, filename), 'r') as fin: # Open file and read the lines
                 lines = fin.readlines()
 
-            archivedf = pd.DataFrame(np.zeros((N, len(archiveColumns))), columns=archiveColumns)
-
             ''' Goes through the lines of the text file data '''
-            m = 0
-            n = 0
-            x = 0
 
             for i in range(len(lines)):
                 if x < N:
@@ -33,30 +35,35 @@ def archiveExtract(directory):
                         coordinates = re.findall("[-+]?[.]?[\d]+(?:,\d\d\d)*[\.]?\d*(?:[eE][-+]?\d+)?", lines[i])
                         info = re.findall("[-+]?[.]?[\d]+(?:,\d\d\d)*[\.]?\d*(?:[eE][-+]?\d+)?", lines[i + 1])
 
+                        # Display which file the source is found in
+                        archivedf.iloc[n, 0] = filename
+
                         # Tildeler koordinaterne til deres plads i DF
-                        archivedf.iloc[n, 1] = float(coordinates[1])
-                        archivedf.iloc[n, 2] = float(coordinates[2])
+                        archivedf.iloc[n, 2] = float(coordinates[1])
+                        archivedf.iloc[n, 3] = float(coordinates[2])
 
                         # Tildeler yderligere info til deres plads i DF
-                        archivedf.iloc[n, 4] = float(info[3])  # obstime
-                        archivedf.iloc[n, 5] = float(info[8])  # time chans start
-                        archivedf.iloc[n, 6] = float(info[9])  # time chans stop
+                        archivedf.iloc[n, 5] = float(info[5])  # cm2
                         archivedf.iloc[n, 7] = float(info[7])  # Significance
-                        archivedf.iloc[n, 8] = float(info[1])  # IJDStart
-                        archivedf.iloc[n, 9] = float(info[2])  # IJDStop
+                        archivedf.iloc[n, 8] = float(info[3])  # obstime
+                        archivedf.iloc[n, 10] = float(info[8])  # time chans start
+                        archivedf.iloc[n, 11] = float(info[9])  # time chans stop
+                        archivedf.iloc[n, 12] = float(info[1])  # IJDStart
+                        archivedf.iloc[n, 13] = float(info[2])  # IJDStop
                         n += 1
 
-                        for j in range(i, len(lines)): # Finds SCW
+                        # Finds the SCW of the source
+                        for j in range(i, len(lines)):
                             if search[1] in lines[j]:
                                 scw = re.findall("[-+]?[.]?[\d]+(?:,\d\d\d)*[\.]?\d*(?:[eE][-+]?\d+)?", lines[j])
-                                archivedf.iloc[m, 0] = int(scw[0])
+                                archivedf.iloc[m, 1] = int(scw[0])
                                 break
 
                         for j in range(i, len(lines)): # Finds Channel Width
                             if search[2] in lines[j]:
                                 chanwid = re.findall("[-+]?[.]?[\d]+(?:,\d\d\d)*[\.]?\d*(?:[eE][-+]?\d+)?", lines[j])
-                                archivedf.iloc[m, 10] = float(chanwid[2])
-                                archivedf.iloc[m, 11] = float(chanwid[2])*archivedf.iloc[m, 5]
+                                archivedf.iloc[m, 6] = float(chanwid[2])  # Channel Width
+                                archivedf.iloc[m, 9] = float(chanwid[2])*archivedf.iloc[m, 10]  # Burst start
                                 break
 
                         for j in range(i, len(lines)): # Finds Name of Source
@@ -64,7 +71,7 @@ def archiveExtract(directory):
                                 break
                             elif "text=" in lines[j]:
                                 s = lines[j]
-                                archivedf.iloc[m, 3] = s[s.find("{")+1:s.find("}")]
+                                archivedf.iloc[m, 4] = s[s.find("{")+1:s.find("}")]
                                 break
                             else:
                                 pass
@@ -72,11 +79,13 @@ def archiveExtract(directory):
                         m += 1
                         x += 1
 
-            ''' Sorts data (Removes zero rows) '''
-            archivedf = archivedf[(archivedf.T != 0).any()]
-
-            archivedf.to_csv("ArchiveCSV/"+directory+filename+".csv", sep=',', encoding="utf-8" , index=False)
             print("Thinking slowly")
+
+    ''' Sorts data (Removes zero rows) '''
+    archivedf = archivedf[(archivedf.T != 0).any()]
+
+    archivedf.to_csv("ArchiveCSV/" + directory + "E03Combined" + ".csv", sep=',', encoding="utf-8", index=False)
+
     print("Done!")
 
 
--- Archive and Burst/BurstSearch.py
+++ Archive and Burst/BurstSearch.py
@@ -1,43 +1,41 @@
 from ArchiveExtract import *
 
 directory = "E03/"
+filename = "E03Combined.csv"
 
 
-def burstSearch(directory):
+def burstSearch(directory, filename):
 
-    for filename in os.listdir("ArchiveCSV/"+directory):
-        if filename.endswith(".csv"): # Goes through all data in the chosen directory
+    archiveCSV = pd.read_csv(os.path.join("ArchiveCSV/" + directory, filename))  # Loads data from ArchiveCSV
+    archivedf = pd.DataFrame(archiveCSV)  # Creates a Dataframe
 
-            archivecsv = pd.read_csv(os.path.join("ArchiveCSV/"+directory, filename))  # Loads data from ArchiveCSV
-            archivedf = pd.DataFrame(archivecsv)  # Creates a Dataframe
+    burstdf = pd.DataFrame(np.zeros((N, len(archiveColumns))), columns=archiveColumns)
 
-            burstdf = pd.DataFrame(np.zeros((N, len(archiveColumns))), columns=archiveColumns)
+    for i in range(len(archivedf.iloc[:, 0])-1):
+        if (83.6280 <= archivedf.iloc[i, 2] <= 83.6361) \
+                    and (22.0151 <= archivedf.iloc[i, 3] <= 22.0199): # Removes Crab Nebula
+            pass
+        elif (((archivedf.iloc[i, 0] == archivedf.iloc[i+1, 0])
+               or abs((archivedf.iloc[i, 1]%10e6 - archivedf.iloc[i+1, 1]%10e6) == 1))
+              and (abs((archivedf.iloc[i, 2]-archivedf.iloc[i+1, 2])) <= 0.003)
+              and (abs((archivedf.iloc[i, 3]-archivedf.iloc[i+1, 3])) <= 0.003)
+              and (archivedf.iloc[i+1, 11] < archivedf.iloc[i, 10]
+                   or archivedf.iloc[i+1, 10] > archivedf.iloc[i, 11])):
+            burstdf.iloc[i, :] = archivedf.iloc[i, :]
+            burstdf.iloc[i+1, :] = archivedf.iloc[i+1, :]
+        else:
+            pass
 
-            for i in range(len(archivedf.iloc[:, 0])-1):
-                if (83.6280 <= archivedf.iloc[i, 1] <= 83.6361) \
-                            and (22.0151 <= archivedf.iloc[i, 2] <= 22.0199): # Removes Crab Nebula
-                    pass
-                elif (((archivedf.iloc[i, 0] == archivedf.iloc[i+1, 0])
-                        or abs((archivedf.iloc[i, 0]%10e6 -archivedf.iloc[i+1, 0]%10e6) == 1))
-                        and (abs((archivedf.iloc[i, 1]-archivedf.iloc[i+1, 1])) <= 0.003)
-                        and (abs((archivedf.iloc[i, 2]-archivedf.iloc[i+1, 2])) <= 0.003)
-                        and (archivedf.iloc[i+1, 6] < archivedf.iloc[i, 5]
-                        or archivedf.iloc[i+1, 5] > archivedf.iloc[i, 6]) ):
-                            burstdf.iloc[i, :] = archivedf.iloc[i, :]
-                            burstdf.iloc[i+1, :] = archivedf.iloc[i+1, :]
-                else:
-                    pass
+    ''' Sorter data (fjerner nul rækker) '''
+    burstdf = burstdf[(burstdf.T != 0).any()]
 
-            ''' Sorter data (fjerner nul rækker) '''
-            burstdf = burstdf[(burstdf.T != 0).any()]
+    burstdf = burstdf.sort_values(by=['SCW', 'time chans start'])
 
-            burstdf = burstdf.sort_values(by=['SCW', 'time chans start'])
+    burstdf.to_csv("BurstCSV/" + directory + "E03CombinedBurst" + ".csv", sep=',', encoding='utf-8', index=False)
 
-            burstdf.to_csv("BurstCSV/"+directory+filename, sep=',', encoding='utf-8', index=False)
-            print("Thinking")
     print("Done!")
 
 
-burstSearch(directory)
+burstSearch(directory, filename)
 
 
